b625ddf710b10d11ab001c45ef0f81b442e587ec
Mac <mac@Maui.local>
2015-09-02 18:34:42 -0400

    Initial Commit

---
+++ AFNetworkigTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AFNetworkigTest
+//
+//  Created by Mac on 9/2/15.
+//  Copyright (c) 2015 Mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AFNetworkigTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AFNetworkigTest
+//
+//  Created by Mac on 9/2/15.
+//  Copyright (c) 2015 Mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
