01a941504d97a3e863a3b38f01b540d48fad47f4
Mac <mac@Maui.local>
2015-09-03 02:57:56 -0400

    weather list JSON complete.

--- AFNetworkigTest/ListViewController.m
+++ AFNetworkigTest/ListViewController.m
@@ -8,15 +8,26 @@
 
 #import "ListViewController.h"
 #import "ListTableViewCell.h"
-#import "RequestHandler.h"
 #import "DetailViewController.h"
+#import "AFNetworking/AFNetworking.h"
+#import "UIImageView+AFNetworking.h"
+#import "Weather.h"
 
+
+//Example URL
+static NSString * const JSONURLString   = @"http://www.raywenderlich.com/demos/weather_sample/weather.php?format=json";
+//static NSString * const XMLURLString    = @"http://www.raywenderlich.com/demos/weather_sample/weather.php?format=xml";
+//static NSString * const PLISTURLString  = @"http://www.raywenderlich.com/demos/weather_sample/weather.php?format=plist";
 
 //Custom cell
 static NSString * const reuseIdentifier = @"listCell";
 
 
 @interface ListViewController ()  <UITableViewDataSource, UITableViewDelegate>
+
+@property (nonatomic, strong) UITableView    *tv;
+@property (nonatomic, strong) NSDictionary   *weather;
+@property (nonatomic, strong) NSMutableArray *weekWeather;
 
 @end
 
@@ -26,23 +37,46 @@ static NSString * const reuseIdentifier = @"listCell";
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    
-    //Set the title and buttons in navigation bar
     self.view.backgroundColor = [UIColor whiteColor];
     
+    [self setNavigationControllerButtons];
+    
+    [self setTableViewandCustomCell];
+    
+    [self setCustomToolBar];
+    
+}
+
+#pragma mark - Set UI
+-(void)setNavigationControllerButtons
+{
+
+    UIBarButtonItem *clearButton = [[UIBarButtonItem alloc] initWithTitle:@"Clear" style:UIBarButtonItemStylePlain target:self action:@selector(clearList)];
+    self.navigationItem.leftBarButtonItem = clearButton;
+    
+    
+//    UIBarButtonItem *apiButton  = [[UIBarButtonItem alloc] initWithTitle:@"API" style:UIBarButtonItemStylePlain target:self action:@selector(setApi)];
+//    self.navigationItem.rightBarButtonItem= apiButton;
+    
+}
+
+-(void)setTableViewandCustomCell
+{
     //Table view
-    UITableView *tv = [[UITableView alloc] initWithFrame:CGRectMake(0,
-                                                                    0,
-                                                                    self.view.frame.size.width,
-                                                                    self.view.frame.size.height-50)
-                                                   style:UITableViewStyleGrouped];
+    self.tv = [[UITableView alloc] initWithFrame:CGRectMake(0,
+                                                            0,
+                                                            self.view.frame.size.width,
+                                                            self.view.frame.size.height-50)
+                                           style:UITableViewStyleGrouped];
     
-    [tv registerClass:[ListTableViewCell class] forCellReuseIdentifier:reuseIdentifier];
-    tv.dataSource = self;
-    tv.delegate   = self;
-    [self.view addSubview:tv];
-    
-    
+    [self.tv registerClass:[ListTableViewCell class] forCellReuseIdentifier:reuseIdentifier];
+    self.tv.dataSource = self;
+    self.tv.delegate   = self;
+    [self.view addSubview:self.tv];
+}
+
+-(void)setCustomToolBar
+{
     //Set Tool bar
     UIToolbar *tb = [[UIToolbar alloc] initWithFrame:CGRectMake(0,
                                                                 self.view.frame.size.height - 55,
@@ -50,21 +84,93 @@ static NSString * const reuseIdentifier = @"listCell";
                                                                 55)];
     tb.backgroundColor = [UIColor blackColor];
     UIBarButtonItem *jsonButton = [[UIBarButtonItem alloc] initWithTitle:@"JSON" style:UIBarButtonItemStylePlain target:self action:@selector(useJSON)];
-
-    UIBarButtonItem *xmlButton  = [[UIBarButtonItem alloc] initWithTitle:@"XML" style:UIBarButtonItemStylePlain target:self action:@selector(useXML)];
     
-    UIBarButtonItem *plistButton= [[UIBarButtonItem alloc] initWithTitle:@"PLIST" style:UIBarButtonItemStylePlain target:self action:@selector(usePLIST)];
+//    UIBarButtonItem *xmlButton  = [[UIBarButtonItem alloc] initWithTitle:@"XML" style:UIBarButtonItemStylePlain target:self action:@selector(useXML)];
+//    
+//    UIBarButtonItem *plistButton= [[UIBarButtonItem alloc] initWithTitle:@"PLIST" style:UIBarButtonItemStylePlain target:self action:@selector(usePLIST)];
+//    
+//    UIBarButtonItem *flexSpace  = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemFlexibleSpace target:self action:nil];
+//    
+//    UIBarButtonItem *clientButton= [[UIBarButtonItem alloc] initWithTitle:@"Client" style:UIBarButtonItemStylePlain target:self action:@selector(clientMode)];
+//    
+//    NSArray *buttonsArray = [NSArray arrayWithObjects:jsonButton, xmlButton, plistButton, flexSpace, clientButton, nil];
     
-    NSArray *buttonsArray = [NSArray arrayWithObjects:jsonButton, xmlButton, plistButton, nil];
+    NSArray *buttonsArray = [NSArray arrayWithObjects:jsonButton, nil];
+    
     [tb setItems:buttonsArray];
     [self.view addSubview:tb];
-
 }
 
+
 #pragma mark - Button Actions
--(void)useJSON
+-(void)clearList
+{
+    self.title = @"";
+    self.weekWeather = nil;
+    [self.tv reloadData];
+}
+
+-(void)setApi
 {
     
+}
+
+-(void)useJSON
+{
+    //prepare url
+    NSURL *url            = [NSURL URLWithString:JSONURLString];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+    
+    AFHTTPRequestOperation *operation = [[AFHTTPRequestOperation alloc] initWithRequest:request];
+    operation.responseSerializer = [AFJSONResponseSerializer serializer];
+    [operation setCompletionBlockWithSuccess:^(AFHTTPRequestOperation *operation, id responseObject)
+     {
+
+         self.weather           = (NSDictionary *)responseObject;
+         NSDictionary *dataDict = self.weather[@"data"];
+         NSArray      *week     = dataDict[@"weather"];
+         self.weekWeather       = [NSMutableArray new];
+         
+         for(NSDictionary *dia in week)
+         {
+             Weather *wd = [Weather new];
+             
+             wd.date                = dia[@"date"];
+             wd.precipMM            = dia[@"precipMM"];
+             wd.tempMaxC            = dia[@"tempMaxC"];
+             wd.tempMaxF            = dia[@"tempMaxF"];
+             wd.tempMinC            = dia[@"tempMinC"];
+             wd.tempMinF            = dia[@"tempMinF"];
+             wd.weatherCode         = dia[@"weatherCode"];
+             NSArray *dd            = dia[@"weatherDesc"];
+             NSDictionary *ddd      = dd[0];
+             wd.weatherDescription  = ddd[@"value"];
+             NSArray *ii            = dia[@"weatherIconUrl"];
+             NSDictionary *iii      = ii[0];
+             wd.weatherIconURL      = iii[@"value"];
+             wd.windDir16Point      = dia[@"winddir16Point"];
+             wd.windDirDegree       = dia[@"winddirDegree"];
+             wd.windSpeedKmph       = dia[@"windspeedKmph"];
+             wd.windSpeedMiles      = dia[@"windspeedMiles"];
+             
+             [self.weekWeather addObject:wd];
+         }
+         
+         self.title = @"Week Weather";
+         [self.tv reloadData];
+         
+     }failure:^(AFHTTPRequestOperation *operation, NSError *error)
+     {
+         UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"Error Retrieving Weather"
+                                                             message:[error localizedDescription]
+                                                            delegate:self
+                                                   cancelButtonTitle:@"Ok"
+                                                   otherButtonTitles:nil];
+         [alertView show];
+         
+     }];
+    
+    [operation start];
 }
 
 -(void)useXML
@@ -77,6 +183,10 @@ static NSString * const reuseIdentifier = @"listCell";
     
 }
 
+-(void)clientMode
+{
+    
+}
 
 
 #pragma mark - Memory
@@ -95,23 +205,34 @@ static NSString * const reuseIdentifier = @"listCell";
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-    return 10;
+    return [self.weekWeather count];
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
     
     ListTableViewCell *cell = (ListTableViewCell *)[tableView dequeueReusableCellWithIdentifier:reuseIdentifier forIndexPath:indexPath];
+    Weather *w = [Weather new];
+    w = self.weekWeather[indexPath.row];
     
-    if (cell == nil)
-    {
-         cell = [[ListTableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:reuseIdentifier];
-    }
+    cell.textLabel.text = w.weatherDescription;
     
-     cell.textLabel.text = [NSString stringWithFormat:@"%li", indexPath.row + 1];
+    NSURL *url = [NSURL URLWithString:w.weatherIconURL];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+    UIImage *placeholderImage = [UIImage imageNamed:@"placeholder"];
+    __weak UITableViewCell *weakCell = cell;
+    [cell.imageView setImageWithURLRequest:request
+                          placeholderImage:placeholderImage
+                                   success:^(NSURLRequest *request, NSHTTPURLResponse *response, UIImage *image) {
+                                       
+                                       weakCell.imageView.image = image;
+                                       [weakCell setNeedsLayout];
+                                       
+                                   } failure:nil];
     
     return cell;
 }
+
 
 #pragma mark - Table view delegate
 
--- AFNetworkigTest/Weather.h
+++ AFNetworkigTest/Weather.h
@@ -11,9 +11,28 @@
 @interface Weather : NSObject
 
 
+
 @property (nonatomic,strong) NSNumber *cloudCover;
 @property (nonatomic,strong) NSNumber *humidity;
 @property (nonatomic,strong) NSDate   *observationTime;
+@property (nonatomic,strong) NSNumber *precipMM;
+@property (nonatomic,strong) NSNumber *pressure;
+@property (nonatomic,strong) NSNumber *tempC;
+@property (nonatomic,strong) NSNumber *tempF;
+@property (nonatomic,strong) NSNumber *visibility;
+@property (nonatomic,strong) NSNumber *weatherCode;
+@property (nonatomic,strong) NSString *windDir16Point;
+@property (nonatomic,strong) NSNumber *windDirDegree;
+@property (nonatomic,strong) NSNumber *windSpeedKmph;
+@property (nonatomic,strong) NSNumber *windSpeedMiles;
+@property (nonatomic,strong) NSString *weatherDescription;
+@property (nonatomic,strong) NSString *weatherIconURL;
+@property (nonatomic,strong) NSDate   *date;
+@property (nonatomic,strong) NSNumber *tempMaxC;
+@property (nonatomic,strong) NSNumber *tempMaxF;
+@property (nonatomic,strong) NSNumber *tempMinC;
+@property (nonatomic,strong) NSNumber *tempMinF;
+
 
 
 @end
